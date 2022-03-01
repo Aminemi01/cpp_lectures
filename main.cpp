f596c2d1d37cc20a08d7cc66c97e4d80fcddc179
Aminemi01 <aminemihoubi0316@gmail.com>
2022-03-01 10:52:48 +0000

    task

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <array>
 #include <cstdio>
 
-int main() {}
+int main()
+{
+        std::array<int, 2> x   = {1, 2};
+        //  for (int i = 0; i < x.size(); i += 1)
+
+        int                sum = 0;
+        for (int i = 0; i < x.size(); i += 1) { sum = sum + x[i]; }
+        std::printf("sum =%d", sum);
+}
